92ee695e2c488d3f1202fcfb1157dade0822a19e
Christian <chrbj-@hotmail.com>
2021-04-16 00:55:11 +0200

    Initial

---
+++ GamePause.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour {
+    
+    public static bool GameIsPaused = false;
+    
+    public SteamVR_Action_Boolean interaction_input;
+    public storyProgressController progressController;
+    public introPlay introPlay;
+
+    private void Start() {
+        Pause();
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (!interaction_input.stateDown) return;
+        if (GameIsPaused) {
+            Resume();
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void Resume() {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        progressController.ProgressStory();
+        introPlay.canStart = true;
+    }
+
+    void Pause() {
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
---
+++ ParticleEffectManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectManager : MonoBehaviour {
+
+    [SerializeField] private ParticleSystem myParticleSystem;
+    [SerializeField] private AudioSource particleSound;
+
+    void Awake() {
+        //particleSystem = GetComponent<ParticleSystem>();
+        StopParticle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartParticle() {
+        if (!myParticleSystem.isPlaying) {
+            myParticleSystem.Play();
+            particleSound.Play();
+        }
+    }
+
+    public void StopParticle() {
+        if (myParticleSystem.isPlaying)
+        {
+            myParticleSystem.Stop();
+            particleSound.Stop();
+        }
+    }
+}
